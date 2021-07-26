102fedf93ec038bf59f195d8832cf74d4ad3cff8
Arif-Im <60347931+Arif-Im@users.noreply.github.com>
2021-07-26 15:49:07 +0800

    v.0.0.1
    - Added game to repository.

---
+++ Wet Work - GP Assignment 3/Assets/Script/Fire.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    [Header("Shooting")]
+    [SerializeField] float startingTimeBetweenShots = 0.1f;
+    [SerializeField] float waterBulletSpeed;
+    [SerializeField] float timeWaterBulletActive = 1f;
+    [SerializeField] WaterBulletBehavior waterBulletPrefab;
+
+    float xDirection;
+    float yDirection;
+    float timeBetweenShots;
+    Rigidbody2D waterBulletRigidbody2D;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBetweenShots > 0)
+        {
+            timeBetweenShots -= Time.deltaTime;
+        }
+        else
+        {
+            xDirection = 0;
+            yDirection = 0;
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                xDirection = 1;
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                xDirection = -1;
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                yDirection = 1;
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                yDirection = -1;
+            }
+
+
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+            {
+                WaterBulletBehavior waterBullet = Instantiate(waterBulletPrefab, transform.position, Quaternion.identity);
+                waterBulletRigidbody2D = waterBullet.GetComponent<Rigidbody2D>();
+                Destroy(waterBullet.gameObject, timeWaterBulletActive);
+                waterBulletRigidbody2D.velocity = new Vector2(xDirection, yDirection).normalized * waterBulletSpeed;
+            }
+
+            timeBetweenShots = startingTimeBetweenShots;
+        }
+    }
+}
---
+++ Wet Work - GP Assignment 3/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Stats")]
+    [SerializeField] float movementSpeed = 10;
+    [SerializeField] float minX = 2, minY = 2, maxX = 5, maxY = 5;
+
+    Rigidbody2D myRigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+
+        myRigidbody2D.velocity = new UnityEngine.Vector2(deltaX, deltaY);
+    }
+
+    public Vector3 GetPlayerVelocity()
+    {
+        return myRigidbody2D.velocity;
+    }
+}
